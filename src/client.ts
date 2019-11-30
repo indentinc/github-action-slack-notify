4de826b8b75a557d8af593c6757b0a153596b43e
Fouad Matin <m@fouad.org>
2019-11-30 13:38:00 -0800

    init: repo

---
+++ src/client.ts
@@ -0,0 +1,171 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { IncomingWebhook, IncomingWebhookSendArguments } from '@slack/webhook'
+
+interface With {
+  status: string
+  mention: '' | 'channel' | 'here'
+  author_name: string
+  only_mention_fail: '' | 'channel' | 'here'
+  username: string
+  icon_emoji: string
+  icon_url: string
+  channel: string
+}
+
+export class Client {
+  private webhook: IncomingWebhook
+  private github?: github.GitHub
+  private with: With
+
+  constructor(props: With) {
+    this.with = props
+
+    if (props.status !== 'custom') {
+      if (process.env.GITHUB_TOKEN === undefined) {
+        throw new Error('Specify secrets.GITHUB_TOKEN')
+      }
+      this.github = new github.GitHub(process.env.GITHUB_TOKEN)
+    }
+
+    if (process.env.SLACK_WEBHOOK_URL === undefined) {
+      throw new Error('Specify secrets.SLACK_WEBHOOK_URL')
+    }
+    this.webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+  }
+
+  public async success(text: string) {
+    const template = await this.payloadTemplate()
+    template.attachments[0].color = 'good'
+    template.text += ':white_check_mark: Succeeded Github Actions\n'
+    template.text += text
+
+    this.send(template)
+  }
+
+  public async fail(text: string) {
+    const template = await this.payloadTemplate()
+    template.attachments[0].color = 'danger'
+    if (this.with.only_mention_fail !== '') {
+      template.text += `<!${this.with.only_mention_fail}> `
+    }
+    template.text += ':no_entry: Failed Github Actions\n'
+    template.text += text
+
+    this.send(template)
+  }
+
+  public async cancel(text: string) {
+    const template = await this.payloadTemplate()
+    template.attachments[0].color = 'warning'
+    template.text += ':warning: Canceled Github Actions\n'
+    template.text += text
+
+    this.send(template)
+  }
+
+  public async send(payload: string | IncomingWebhookSendArguments) {
+    core.debug(JSON.stringify(github.context, null, 2))
+    await this.webhook.send(payload)
+    core.debug('send message')
+  }
+
+  private async payloadTemplate() {
+    let text = ''
+    if (this.with.mention !== '') {
+      text += `<!${this.with.mention}> `
+    }
+
+    return {
+      text: text,
+      attachments: [
+        {
+          color: '',
+          author_name: this.with.author_name,
+          username: this.with.username,
+          icon_emoji: this.with.icon_emoji,
+          icon_url: this.with.icon_url,
+          channel: this.with.channel,
+          fields: await this.fields()
+        }
+      ]
+    }
+  }
+
+  private async fields() {
+    if (this.github === undefined) {
+      throw Error('Specify secrets.GITHUB_TOKEN')
+    }
+    const { sha } = github.context
+    const { owner, repo } = github.context.repo
+    const commit = await this.github.repos.getCommit({ owner, repo, ref: sha })
+    const { author } = commit.data.commit
+
+    return [
+      this.repo,
+      {
+        title: 'message',
+        value: commit.data.commit.message,
+        short: true
+      },
+      this.commit,
+      {
+        title: 'author',
+        value: `${author.name}<${author.email}>`,
+        short: true
+      },
+      this.action,
+      this.eventName,
+      this.ref,
+      this.workflow
+    ]
+  }
+
+  private get commit() {
+    const { sha } = github.context
+    const { owner, repo } = github.context.repo
+
+    return {
+      title: 'commit',
+      value: `<https://github.com/${owner}/${repo}/commit/${sha}|${sha}>`,
+      short: true
+    }
+  }
+
+  private get repo() {
+    const { owner, repo } = github.context.repo
+
+    return {
+      title: 'repo',
+      value: `<https://github.com/${owner}/${repo}|${owner}/${repo}>`,
+      short: true
+    }
+  }
+
+  private get action() {
+    const { sha } = github.context
+    const { owner, repo } = github.context.repo
+
+    return {
+      title: 'action',
+      value: `<https://github.com/${owner}/${repo}/commit/${sha}/checks|action>`,
+      short: true
+    }
+  }
+
+  private get eventName() {
+    return {
+      title: 'eventName',
+      value: github.context.eventName,
+      short: true
+    }
+  }
+
+  private get ref() {
+    return { title: 'ref', value: github.context.ref, short: true }
+  }
+
+  private get workflow() {
+    return { title: 'workflow', value: github.context.workflow, short: true }
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import * as core from '@actions/core'
+import { Client } from './client'
+import { IncomingWebhookSendArguments } from '@slack/webhook'
+
+async function run() {
+  try {
+    let status: string = core.getInput('status', { required: true })
+    status = status.toLowerCase()
+    const mention = core.getInput('mention') as '' | 'channel' | 'here'
+    const author_name = core.getInput('author_name')
+    const only_mention_fail = core.getInput('only_mention_fail') as
+      | ''
+      | 'channel'
+      | 'here'
+    const text = core.getInput('text')
+    const username = core.getInput('username')
+    const icon_emoji = core.getInput('icon_emoji')
+    const icon_url = core.getInput('icon_url')
+    const channel = core.getInput('channel')
+    const rawPayload = core.getInput('payload')
+
+    core.debug(`status: ${status}`)
+    core.debug(`mention: ${mention}`)
+    core.debug(`author_name: ${author_name}`)
+    core.debug(`only_mention_fail: ${only_mention_fail}`)
+    core.debug(`text: ${text}`)
+    core.debug(`username: ${username}`)
+    core.debug(`icon_emoji: ${icon_emoji}`)
+    core.debug(`icon_url: ${icon_url}`)
+    core.debug(`channel: ${channel}`)
+    core.debug(`rawPayload: ${rawPayload}`)
+
+    const client = new Client({
+      status,
+      mention,
+      author_name,
+      only_mention_fail,
+      username,
+      icon_emoji,
+      icon_url,
+      channel
+    })
+
+    switch (status) {
+      case 'success':
+        await client.success(text)
+        break
+      case 'failure':
+        await client.fail(text)
+        break
+      case 'cancelled':
+        await client.cancel(text)
+        break
+      case 'custom':
+        var payload: IncomingWebhookSendArguments = eval(
+          `payload = ${rawPayload}`
+        )
+        await client.send(payload)
+        break
+      default:
+        throw new Error(
+          'You can specify success or failure or cancelled or custom'
+        )
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
